50160eeb41fd109e158698f0e8a1c8ca16bab3a2
Joshua Vial <joshua@enspiral.com>
2017-09-29 15:04:19 +1300

    refactoring client and server

--- src/client.js
+++ src/client.js
@@ -7,21 +7,24 @@ function startClient(url, port, log, rl) {
 
   ws.on("open", () => {
     rl.prompt()
-    rl.on("line", (input) => {
-      if (["bye", "exit", "/q"].indexOf(input) != -1) {
-        rl.close()
-        ws.close()
-      } else {
-        ws.send(input)
-        rl.prompt()
-      }
-    })
+    rl.on("line", (input) => { processLine(input, rl, ws) })
+
     ws.on('message', (message) => {
       log(`\n${message}`)
       rl.prompt()
     })
   })
   return ws
+}
+
+function processLine(input, rl, ws) {
+  if (["bye", "exit", "/q"].indexOf(input) != -1) {
+    rl.close()
+    ws.close()
+  } else {
+    ws.send(input)
+    rl.prompt()
+  }
 }
 
 module.exports = startClient
--- src/server.js
+++ src/server.js
@@ -5,18 +5,17 @@ function startServer(port, log) {
   log(`Tech gym slack server running on ws://localhost:${port}`)
   wss.on("connection", (ws) => {
     ws.send("Oh! Let's go!")
-    ws.on("message", (message) => {
-      log(`received: ${message}`)
-      if (message == "echo") {
-        ws.send("echo")
-      }
-    })
-    ws.on("close", () => {
-      log("Another one bites the dust")
-    })
+    ws.on("message", (message) => { receiveMessage(message, ws, log) })
+    ws.on("close", () => { log("Another one bites the dust") })
   })
-
   return wss
+}
+
+function receiveMessage(message, ws, log) {
+  log(`received: ${message}`)
+  if (message == "echo") {
+    ws.send("echo")
+  }
 }
 
 module.exports = startServer
